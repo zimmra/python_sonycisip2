d55406aa4b04efdd4e737c2fca2da0616baf1c3e
zimmra <paytonz@icloud.com>
2023-11-01 15:46:57 -0700

    async

--- python_sonycisip2/python_sonycisip2.py
+++ python_sonycisip2/python_sonycisip2.py
@@ -2,29 +2,42 @@
 from commandset import commands_dict
 from variables import variables_dict
 
+# Importing required modules
 import socket
 import json
 import asyncio
+import logging
+
+# Defining constants for message types
+SET = "set"
+GET = "get"
+NOTIFY = "notify"
 
 class SonyCISIP2:
-    def __init__(self, host, port=33336):
+    def __init__(self, host, port=33336, loop=None):
         """
         Initialize the SonyCISIP2 class with the IP address and port of the Sony receiver.
         """
         self.host = host
         self.port = port
+        self.loop = loop if loop else asyncio.get_event_loop()
         self.socket = None
+        self.logger = logging.getLogger(__name__)
 
-    def connect(self):
+    async def connect(self):
         """
-        Establish a TCP socket connection to the Sony receiver.
+        Asynchronously establish a TCP socket connection to the Sony receiver.
         """
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
+        try:
+            self.socket = await self.loop.sock_connect(socket.AF_INET, socket.SOCK_STREAM, (self.host, self.port))
+        except Exception as e:
+            self.logger.error(f"Failed to connect: {e}")
+            return False
+        return True
 
-    def send_message(self, message_type, feature, value=None):
+    async def send_message(self, message_type, feature, value=None):
         """
-        Send a message to the Sony receiver.
+        Asynchronously send a message to the Sony receiver.
         Constructs a JSON message based on the type, feature, and value provided.
         """
         message = {
@@ -35,50 +48,50 @@ class SonyCISIP2:
             message["value"] = value
 
         json_message = json.dumps(message)
-        self.socket.sendall(json_message.encode('utf-8'))
+        try:
+            await self.loop.sock_sendall(self.socket, json_message.encode('utf-8'))
+        except Exception as e:
+            self.logger.error(f"Failed to send message: {e}")
 
-    def receive_message(self):
+    async def receive_message(self):
         """
-        Receive a message from the Sony receiver.
+        Asynchronously receive a message from the Sony receiver.
         Returns the message as a JSON object.
         """
-        data = self.socket.recv(1024)
+        try:
+            data = await self.loop.sock_recv(self.socket, 1024)
+        except Exception as e:
+            self.logger.error(f"Failed to receive message: {e}")
+            return None
         return json.loads(data.decode('utf-8'))
 
-    def send_and_receive_message(self, message_type, feature, value=None):
+    async def send_and_receive_message(self, message_type, feature, value=None):
         """
-        Send a message to the Sony receiver and wait for a reply.
+        Asynchronously send a message to the Sony receiver and wait for a reply.
         Constructs a JSON message based on the type, feature, and value provided.
         Returns the received response.
         """
-        self.send_message(message_type, feature, value)
-        return self.receive_message()
+        await self.send_message(message_type, feature, value)
+        return await self.receive_message()
 
-
-    def set_feature(self, feature, value):
+    async def set_feature(self, feature, value):
         """
-        Send a 'set' command to the Sony receiver to set a feature to a specific value.
+        Asynchronously send a 'set' command to the Sony receiver to set a feature to a specific value.
         Returns the result as 'ACK', 'NAK', or 'ERR'.
         """
-        self.send_message("set", feature, value)
-        response = self.receive_message()
-        result_value = response.get("value", "Unknown Response")
-        if result_value in ["ACK", "NAK", "ERR"]:
-            return result_value
-        else:
-            return "Unknown Response"
+        await self.send_message(SET, feature, value)
+        response = await self.receive_message()
+        result_value = response.get("value", "Unknown Response") if response else "Unknown Response"
+        return result_value
 
-    commands_dict = commands_dict
-    variables_dict = variables_dict
-
-    def get_feature(self, feature):
+    async def get_feature(self, feature):
         """
-        Send a 'get' command to the Sony receiver to get the value of a feature.
+        Asynchronously send a 'get' command to the Sony receiver to get the value of a feature.
         Returns the value of the feature.
         """
-        self.send_message("get", feature)
-        response = self.receive_message()
-        return response.get("value", "Unknown Value")
+        await self.send_message(GET, feature)
+        response = await self.receive_message()
+        return response.get("value", "Unknown Value") if response else "Unknown Value"
 
     async def listen_for_notifications(self, callback=None):
         """
@@ -86,14 +99,13 @@ class SonyCISIP2:
         If a callback function is provided, it will be invoked with the notification message.
         """
         while True:
-            message = self.receive_message()
-            if message["type"] == "notify":
+            message = await self.receive_message()
+            if message and message["type"] == NOTIFY:
                 if callback:
                     callback(message)
-                else:
-                    print(f"Received notification: {message}")
             await asyncio.sleep(0.1)  # To prevent high CPU usage
 
+# Additional utility function remains the same
 def replace_command_placeholders(command_str, variables_dict):
     """
     Replaces placeholders in the command string with actual values from the variables dictionary.
